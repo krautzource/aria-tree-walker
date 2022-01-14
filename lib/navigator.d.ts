463fdec664bb450c54e401e4f5bc17cecb921a31
Peter Krautzberger <p.krautzberger@gmail.com>
2022-01-14 16:55:45 +0100

    chore: regenerate build, docs

--- lib/navigator.d.ts
+++ lib/navigator.d.ts
@@ -1,6 +1,6 @@
 /**
  * Attaches a navigator to the DOM node.
  * @param {Node} node - The target node.
- * @param {AbortSignal} [abortControllerSignal] - An optional signal from an AbortController.
+ * @param {AbortSignal} [abortControllerSignal] - An (optional) signal from an AbortController.
  */
 export function attachNavigator(node: Node, abortControllerSignal?: AbortSignal): void;
