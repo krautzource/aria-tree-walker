d104b0585ad1d1d9fdf0661ed5297a14689fd048
Peter Krautzberger <p.krautzberger@gmail.com>
2026-04-09 17:23:41 +0200

    test: move to node:test and puppeteer
    Revises test setup by replacing ava with node:test
    and playwright with puppeteer.
    - package.json
      - add type=module
      - update test script
      - remove ava and playwright
      - update polka and sirv
      - add puppeteer
    - test/test.js
      - move to esm
      - adjust setup for puppeteer
      - switch tests to node:test
      - add teardown (mildly hacky)
    
    Resolves #61

--- test/test.js
+++ test/test.js
@@ -1,77 +1,72 @@
-const sirv = require('sirv');
-const polka = require('polka');
+import test from 'node:test';
+import sirv from 'sirv';
+import polka from 'polka';
+const __dirname = import.meta.dirname;
+
+import puppeteer from 'puppeteer';
 
 const assets = sirv(__dirname + '/../', {
   maxAge: 1,
   immutable: true,
 });
 
-polka()
+const app = polka()
+app
   .use(assets)
   .listen(8080, (err) => {
     if (err) throw err;
   });
 
-const { chromium } = require('playwright');
-const test = require('ava').default;
-const browserPromise = chromium.launch({
+const browser = await puppeteer.launch({
   args: ['--enable-experimental-web-platform-features'],
 });
 
-async function pageMacro(t, callback) {
-  const browser = await browserPromise;
-  const page = await browser.newPage();
-  try {
-    await callback(t, page);
-  } finally {
-    await page.close();
-  }
-}
+const page = await browser.newPage();
 
-test('check test setup', pageMacro, async (t, page) => {
-  await page.goto('localhost:8080/test/');
-  t.is(await page.title(), 'ARIA tree walker Test');
+test('check test setup', async (t) => {
+  await page.goto('http://localhost:8080/test/');
+  t.assert.equal(await page.title(), 'ARIA tree walker Test');
 });
 
-test('catch errors', pageMacro, async (t, page) => {
+test('catch errors', async (t) => {
   page.on('pageerror', (exception) => {
     console.log(`Uncaught exception: "${exception}"`);
-    t.fail();
+    t.assert.fail();
   });
-  await page.goto('localhost:8080/test/');
+  await page.goto('http://localhost:8080/test/');
   await page.evaluate(() => {
     document.querySelector('[aria-label="bad tree 2"]').focus();
     document.querySelector('[aria-label="bad tree 3"]').focus();
   });
-  t.pass();
+  t.assert.ok(true);
 });
 
-test('focus and arrow down', pageMacro, async (t, page) => {
-  await page.goto('localhost:8080/test/');
+test('focus and arrow down', async (t) => {
+  await page.goto('http://localhost:8080/test/');
   await page.keyboard.press('Tab');
   await page.keyboard.press('ArrowDown');
   const activedescendantId = await page.evaluate(() => {
     return document.activeElement.getAttribute('data-owns-id');
   });
-  t.is(activedescendantId, 'treeitem1');
+  t.assert.equal(activedescendantId, 'treeitem1');
   const treeActivedescendantProp = await page.evaluate(() => {
     return document.activeElement.closest('[role="tree"]').getAttribute('data-activedescendant');
   });
-  t.is(treeActivedescendantProp, activedescendantId);
+  t.assert.equal(treeActivedescendantProp, activedescendantId);
   await page.keyboard.press('Tab');
   const treeActivedescendantPropAfterFocusOut = await page.evaluate(() => {
     return document.querySelector('[aria-label="test tree 1"]').getAttribute('data-activedescendant');
   });
-  t.is(treeActivedescendantPropAfterFocusOut, '');
+  t.assert.equal(treeActivedescendantPropAfterFocusOut, '');
 });
 
-test('links: check tabindex', pageMacro, async (t, page) => {
-  await page.goto('localhost:8080/test/');
+test('links: check tabindex', async (t) => {
+  await page.goto('http://localhost:8080/test/');
   let tabindex = await page.evaluate(() => {
     return document
       .querySelector('a').getAttribute('tabindex');
   });
-  t.is(tabindex, '-1');
+  t.assert.equal(tabindex, '-1');
   await page.keyboard.press('Tab');
   await page.keyboard.press('Tab');
   await page.keyboard.press('ArrowDown');
@@ -80,11 +75,11 @@ test('links: check tabindex', pageMacro, async (t, page) => {
     return document
       .querySelector('a').getAttribute('tabindex');
   });
-  t.is(tabindex, '0');
+  t.assert.equal(tabindex, '0');
 });
 
-test('links: activating  with ENTER', pageMacro, async (t, page) => {
-  await page.goto('localhost:8080/test/');
+await test('links: activating  with ENTER', async (t) => {
+  await page.goto('http://localhost:8080/test/');
   await page.keyboard.press('Tab');
   await page.keyboard.press('Tab');
   await page.keyboard.press('ArrowDown');
@@ -92,11 +87,11 @@ test('links: activating  with ENTER', pageMacro, async (t, page) => {
   await page.keyboard.press('Enter');
   await page.waitForNavigation({ waitUntil: 'load' });
   const location = await page.evaluate(() => document.location.toString());
-  t.is(location, 'https://example.com/');
+  t.assert.equal(location, 'https://example.com/');
 });
 
-test('links: activating with SPACE', pageMacro, async (t, page) => {
-  await page.goto('localhost:8080/test/');
+await test('links: activating with SPACE', async (t) => {
+  await page.goto('http://localhost:8080/test/');
   await page.keyboard.press('Tab');
   await page.keyboard.press('Tab');
   await page.keyboard.press('ArrowDown');
@@ -104,11 +99,11 @@ test('links: activating with SPACE', pageMacro, async (t, page) => {
   await page.keyboard.press('Space');
   await page.waitForNavigation({ waitUntil: 'load' });
   const location = await page.evaluate(() => document.location.toString());
-  t.is(location, 'https://example.com/');
+  t.assert.equal(location, 'https://example.com/');
 });
 
-test('links: faux-link', pageMacro, async (t, page) => {
-  await page.goto('localhost:8080/test/');
+await test('links: faux-link', async (t) => {
+  await page.goto('http://localhost:8080/test/');
   await page.keyboard.press('Tab');
   await page.keyboard.press('Tab');
   await page.keyboard.press('ArrowDown');
@@ -117,47 +112,47 @@ test('links: faux-link', pageMacro, async (t, page) => {
   await page.keyboard.press('Space');
   await page.waitForNavigation({ waitUntil: 'load' });
   const location = await page.evaluate(() => document.location.toString());
-  t.is(location, 'https://example.com/');
+  t.assert.equal(location, 'https://example.com/');
 });
 
-test('highlighting: tree', pageMacro, async (t, page) => {
-  await page.goto('localhost:8080/test/');
+await test('highlighting: tree', async (t) => {
+  await page.goto('http://localhost:8080/test/');
   await page.keyboard.press('Tab');
   let classnameTree = await page.evaluate(() => document.querySelector('[aria-label="test tree 1"]').className);
-  t.is(classnameTree, 'is-highlight is-activedescendant');
+  t.assert.equal(classnameTree, 'is-highlight is-activedescendant');
   let classnameTreeitem = await page.evaluate(() => document.querySelector('[aria-label="test tree 1"] [data-owns-id="treeitem1"]').className);
-  t.is(classnameTreeitem, 'is-highlight');
+  t.assert.equal(classnameTreeitem, 'is-highlight');
   let ariahiddenTreeitem = await page.evaluate(() => document.querySelector('[aria-label="test tree 1"] [data-owns-id="treeitem1"]').getAttribute('aria-hidden'));
-  t.is(ariahiddenTreeitem, 'true');
+  t.assert.equal(ariahiddenTreeitem, 'true');
   await page.keyboard.press('ArrowDown');
   classnameTree = await page.evaluate(() => document.querySelector('[aria-label="test tree 1"]').className);
   classnameTreeitem = await page.evaluate(() => document.querySelector('[aria-label="test tree 1"] [data-owns-id="treeitem1"]').className)
-  t.is(classnameTree, '');
-  t.is(classnameTreeitem, 'is-highlight is-activedescendant');
+  t.assert.equal(classnameTree, '');
+  t.assert.equal(classnameTreeitem, 'is-highlight is-activedescendant');
   ariahiddenTreeitem = await page.evaluate(() => document.querySelector('[aria-label="test tree 1"] [data-owns-id="treeitem1"]').hasAttribute('aria-hidden'));
-  t.is(ariahiddenTreeitem, false);
+  t.assert.equal(ariahiddenTreeitem, false);
   await page.keyboard.press('ArrowUp');
   ariahiddenTreeitem = await page.evaluate(() => document.querySelector('[aria-label="test tree 1"] [data-owns-id="treeitem1"]').getAttribute('aria-hidden'));
-  t.is(ariahiddenTreeitem, 'true');
+  t.assert.equal(ariahiddenTreeitem, 'true');
 });
 
-test('highlighting: subtreeitem not descendant', pageMacro, async (t, page) => {
-  await page.goto('localhost:8080/test/');
+await test('highlighting: subtreeitem not descendant', async (t) => {
+  await page.goto('http://localhost:8080/test/');
   await page.keyboard.press('Tab');
   await page.keyboard.press('ArrowDown');
   const classname = await page.evaluate(() => document.querySelector('[aria-label="test tree 1"] [data-owns-id="treeitem3"]').className);
-  t.is(classname, 'is-highlight');
+  t.assert.equal(classname, 'is-highlight');
 });
 
-test('abort (remove) navigator', pageMacro, async (t, page) => {
-  await page.goto('localhost:8080/test/abortSignal.html');
+await test('abort (remove) navigator', async (t) => {
+  await page.goto('http://localhost:8080/test/abortSignal.html');
   await page.keyboard.press('Tab');
   // navigate
   await page.keyboard.press('ArrowDown');
   const treeActivedescendantProp = await page.evaluate(() => {
     return document.activeElement.closest('[role="tree"]').getAttribute('data-activedescendant');
   });
-  t.is(treeActivedescendantProp, 'treeitem1');
+  t.assert.equal(treeActivedescendantProp, 'treeitem1');
   // abort navigator
   await page.evaluate(() => window.controller.abort());
   // attempt navigation
@@ -166,5 +161,11 @@ test('abort (remove) navigator', pageMacro, async (t, page) => {
   const treeActivedescendantProp2 = await page.evaluate(() => {
     return document.querySelector('[role="tree"]').getAttribute('data-activedescendant');
   });
-  t.is(treeActivedescendantProp2, treeActivedescendantProp);
+  t.assert.equal(treeActivedescendantProp2, treeActivedescendantProp);
+});
+
+test('teardown', async (t) => {
+  await page.close();
+  await browser.close();
+  await app.server.close();
 });
