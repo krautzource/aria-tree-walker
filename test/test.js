e62f4c868a7426af5d8dcefb47ca7989e4f62afd
Peter Krautzberger <p.krautzberger@gmail.com>
2026-04-22 16:10:57 +0200

    feat!: initial rewrite towards mobile AT support
    Rewrites abstractTree and navigator to prepare for AT support,
    in particular enabling "open" exploration (where explored
    nodes and their siblings remain visible unless "up" exploration
    collapses the tree again). Also adds support for focus handling
    moving within&into the tree (e.g. tab, link targets)
    
    - dist/
      - refresh build (to new rollup setup, cf 6d0875d)
    - lib/abstractTree.js
      - jsdoc: update and add comments
      - extractAbstractTree()
        - update failure warning
      - recurseNodeToExtractTree()
        - pass node to abstractNode constructor
        - update failure warning
      - abstractNode class
        - constructor()
          - change argument to HTMLElement
           - add this.domnode (pointer to DOM node)
           - add this.visible (boolean)
          - adjust .name (in response to argument changes)
        - abstractTree class
          - constructor()
            - set root to be visible
          - up()
            - set new active node to visible=true
            - set its children to visible=false
          - down()
            - set old active node to visible=false
            - set its children to visible=true
          - left()
            - generalize movement across "parent boundary"
              (cf. findNextVisibleCousin() below)
          - right()
            - generalize movement across "parent boundary"
              (cf. findNextVisibleCousin() below)
          - add search()
            - searches the tree from a given abstractNode
              checking against a given function, possibly
              in reverse direction
          - add findByName()
            - wrapper to find an abstractNode by name
              (used in navigator.js)
          - add findNextVisibleCousin
            - searches the tree for a viable "cousin" node,
              i.e. the closest visible node in the tree
              (in the specified direction)
    - lib/navigator.js
      - navigator class
        - constructor()
          - return early if no tree structure can be found
          - on focusin, use new enableFocusedNode() (cf. below)
          - remove focusout event handler
        - remove active() (using this.tree.active is shorter
          and clearer)
        - move()
          - switch to moveUp() and moveDown (cf. below)
        - add enableFocusdNode()
          - enables a given node while disabling its
            ancestor or descendant nodes
        - add enableNode()
          - handles ARIA-side of making a node visible
        - add disableNode()
          - handles ARIA-side of making a node not-visible
        - moveDown()
          - disables current DOM node
          - enables its (tree-)children
          - moves down in the abstractTree
        - moveUp()
          - moves up in the abstractTree
          - enables now current DOM node
          - disables its (tree-)children
        - highlighSubtree()
          - remove aria-hidden handling (now via disableNode())
        - highlight()
          - simplify DOM node identification
          - remove tabindex handling
          - remove data-activedescendant handling
          - remove aria-hidden handling
    - tests
      - test/abortSignal.html
        - update to new markup expectation (role, data-label, root markup)
        - adjust imports and selector
      - test/index.html
        - update to new markup expectations (as above, link exception)
        - adjust imports and selector
      - test/test.js
        - remove data-activedescendants tests
        - adjust to new markup expectations (data-label etc)
    
    NOTE: more tests will be added separately
    Part of #44

--- test/test.js
+++ test/test.js
@@ -49,15 +49,6 @@ test('focus and arrow down', async (t) => {
     return document.activeElement.getAttribute('data-owns-id');
   });
   t.assert.equal(activedescendantId, 'treeitem1');
-  const treeActivedescendantProp = await page.evaluate(() => {
-    return document.activeElement.closest('[role="tree"]').getAttribute('data-activedescendant');
-  });
-  t.assert.equal(treeActivedescendantProp, activedescendantId);
-  await page.keyboard.press('Tab');
-  const treeActivedescendantPropAfterFocusOut = await page.evaluate(() => {
-    return document.querySelector('[aria-label="test tree 1"]').getAttribute('data-activedescendant');
-  });
-  t.assert.equal(treeActivedescendantPropAfterFocusOut, '');
 });
 
 await test('links: activating  with ENTER', async (t) => {
@@ -75,21 +66,21 @@ await test('links: activating  with ENTER', async (t) => {
 await test('highlighting: tree', async (t) => {
   await page.goto('http://localhost:8080/test/');
   await page.keyboard.press('Tab');
-  let classnameTree = await page.evaluate(() => document.querySelector('[aria-label="test tree 1"]').className);
+  let classnameTree = await page.evaluate(() => document.querySelector('[data-label="test tree 1"]').className);
   t.assert.equal(classnameTree, 'is-highlight is-activedescendant');
-  let classnameTreeitem = await page.evaluate(() => document.querySelector('[aria-label="test tree 1"] [data-owns-id="treeitem1"]').className);
+  let classnameTreeitem = await page.evaluate(() => document.querySelector('[data-label="test tree 1"] [data-owns-id="treeitem1"]').className);
   t.assert.equal(classnameTreeitem, 'is-highlight');
-  let ariahiddenTreeitem = await page.evaluate(() => document.querySelector('[aria-label="test tree 1"] [data-owns-id="treeitem1"]').getAttribute('aria-hidden'));
+  let ariahiddenTreeitem = await page.evaluate(() => document.querySelector('[data-label="test tree 1"] [data-owns-id="treeitem1"]').getAttribute('aria-hidden')); //NOTE: this is (now) simply checking that the HTML source (which should start with aria-hidden) has not been changed at this stage
   t.assert.equal(ariahiddenTreeitem, 'true');
   await page.keyboard.press('ArrowDown');
-  classnameTree = await page.evaluate(() => document.querySelector('[aria-label="test tree 1"]').className);
-  classnameTreeitem = await page.evaluate(() => document.querySelector('[aria-label="test tree 1"] [data-owns-id="treeitem1"]').className)
+  classnameTree = await page.evaluate(() => document.querySelector('[data-label="test tree 1"]').className);
+  classnameTreeitem = await page.evaluate(() => document.querySelector('[data-label="test tree 1"] [data-owns-id="treeitem1"]').className)
   t.assert.equal(classnameTree, '');
   t.assert.equal(classnameTreeitem, 'is-highlight is-activedescendant');
-  ariahiddenTreeitem = await page.evaluate(() => document.querySelector('[aria-label="test tree 1"] [data-owns-id="treeitem1"]').hasAttribute('aria-hidden'));
+  ariahiddenTreeitem = await page.evaluate(() => document.querySelector('[data-label="test tree 1"] [data-owns-id="treeitem1"]').hasAttribute('aria-hidden'));
   t.assert.equal(ariahiddenTreeitem, false);
   await page.keyboard.press('ArrowUp');
-  ariahiddenTreeitem = await page.evaluate(() => document.querySelector('[aria-label="test tree 1"] [data-owns-id="treeitem1"]').getAttribute('aria-hidden'));
+  ariahiddenTreeitem = await page.evaluate(() => document.querySelector('[data-label="test tree 1"] [data-owns-id="treeitem1"]').getAttribute('aria-hidden'));
   t.assert.equal(ariahiddenTreeitem, 'true');
 });
 
@@ -97,7 +88,7 @@ await test('highlighting: subtreeitem not descendant', async (t) => {
   await page.goto('http://localhost:8080/test/');
   await page.keyboard.press('Tab');
   await page.keyboard.press('ArrowDown');
-  const classname = await page.evaluate(() => document.querySelector('[aria-label="test tree 1"] [data-owns-id="treeitem3"]').className);
+  const classname = await page.evaluate(() => document.querySelector('[data-owns-id="root"] [data-owns-id="treeitem3"]').className);
   t.assert.equal(classname, 'is-highlight');
 });
 
@@ -107,7 +98,7 @@ await test('abort (remove) navigator', async (t) => {
   // navigate
   await page.keyboard.press('ArrowDown');
   const treeActivedescendantProp = await page.evaluate(() => {
-    return document.activeElement.closest('[role="tree"]').getAttribute('data-activedescendant');
+    return document.activeElement.getAttribute('data-owns-id');
   });
   t.assert.equal(treeActivedescendantProp, 'treeitem1');
   // abort navigator
@@ -116,9 +107,9 @@ await test('abort (remove) navigator', async (t) => {
   await page.keyboard.press('ArrowUp');
   // note failure to navigate
   const treeActivedescendantProp2 = await page.evaluate(() => {
-    return document.querySelector('[role="tree"]').getAttribute('data-activedescendant');
+    return document.querySelector('[data-owns-id="root"]').getAttribute('role');
   });
-  t.assert.equal(treeActivedescendantProp2, treeActivedescendantProp);
+  t.assert.equal(treeActivedescendantProp2, 'none');
 });
 
 test('teardown', async (t) => {
