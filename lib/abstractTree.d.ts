bdab7f68db5f7d87092ef9f64447870e7465e8e7
Peter Krautzberger <p.krautzberger@gmail.com>
2026-04-24 12:17:51 +0200

    fix: update type declarations
    - package.json
      - upgrade declarations script
        - add abstractTree.js generation
        - fix name of copy to ./dist
      - upgrade typescript
    - dist/
      - regenerate declaration (fixes filename)
    - lib/
      - regenerate abstractTree.js declaration
    
    Part of #62

--- lib/abstractTree.d.ts
+++ lib/abstractTree.d.ts
@@ -1,12 +1,52 @@
-export function extractAbstractTree(node: Node): void | abstractTree;
+export function extractAbstractTree(node: Node): abstractTree;
 declare class abstractTree {
-    constructor(root: any);
-    root: any;
-    active: any;
+    /**
+     *
+     * @param {abstractNode} root
+     */
+    constructor(root: abstractNode);
+    root: abstractNode;
+    active: abstractNode;
     up(): void;
     down(): void;
     left(): void;
     right(): void;
-    activateLink(): void;
+    /**
+     *
+     * @param {abstractNode} abstractNode
+     * @param {Function} filterfuncton - to test if abstractNode is a match for the search
+     * @param {Boolean} inReverse - if true, search children in reverse order
+     * @returns
+     */
+    search(abstractNode: abstractNode, filterfuncton: Function, inReverse?: boolean): any;
+    /**
+     *
+     * @param {abstractNode} abstractNode
+     * @param {String} name - the name of a descendant abstractNode
+     * @returns
+     */
+    findByName(abstractNode: abstractNode, name: string): any;
+    /**
+     * Finds the next visible "cousin" node (which may be a sibling)
+     * @param {abstractNode} abstractNode
+     * @param {Boolean} inReverse - move left/right
+     * @returns
+     */
+    findNextVisibleCousin(abstractNode: abstractNode, inReverse?: boolean): abstractNode;
+}
+/**
+ * The basic tree for the light walker.
+ */
+declare class abstractNode {
+    /**
+     * @constructor
+     * @param {HTMLElement} node
+     */
+    constructor(node: HTMLElement);
+    domnode: HTMLElement;
+    visible: boolean;
+    name: string | null;
+    parent: any;
+    children: any[];
 }
 export {};
